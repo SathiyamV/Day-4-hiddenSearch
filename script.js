124a44398035c5533398c85ca0103bda3e136e7f
SathiyamV <88426005+SathiyamV@users.noreply.github.com>
2023-09-07 23:04:59 +0530

    3 files modified

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
-console.log('Hello!');
+const search = document.querySelector('.search');
+const input = document.querySelector('.input');
+const btn = document.querySelector('.btn');
+
+btn.addEventListener('click', () => {
+  search.classList.toggle('active');
+  input.focus();
+});
